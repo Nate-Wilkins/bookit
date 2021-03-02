b5b1c14f8fcb3099a7d4525fbda397cd459b6856
nate-wilkins <nate-wilkins@code-null.com>
2021-03-01 23:40:11 -0800

    Add 'add' support

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,228 @@
 use anyhow::{Context, Result};
-use dirs;
+use clap::{App, Arg, SubCommand};
+use serde::{Deserialize, Serialize};
+use shellexpand;
 use std;
-use yaml_rust::YamlLoader;
+use std::collections::BTreeMap;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Config {
+    bookmarks: BTreeMap<String, ConfigBookmark>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct ConfigBookmark {
+    url: String,
+    tags: Vec<String>,
+}
 
 /*
  * Bookmarks manager.
- * - View
- * - TODO@nw: Add
- *            docket add --name "Some Bookmark" --url "https://google.com" --tags search --tags nw
- * - TODO@nw: Delete
  */
 fn main() {
-    // let args: Vec<String> = std::env::args().collect();
+    // Command line interface.
+    let args = App::new("docket")
+        .version("1.0")
+        .author("Nathaniel Wilkins <nate-wilkins@code-null.com>")
+        .about("A bookmarks manager!")
+        .arg(
+            Arg::with_name("config")
+                .long("config")
+                .required(false)
+                .default_value("~/.docket")
+                .help("configuration file to use"),
+        )
+        .subcommand(SubCommand::with_name("view").about("view bookmarks"))
+        .subcommand(SubCommand::with_name("list-tags").about("lists all tags recorded"))
+        .subcommand(
+            SubCommand::with_name("add")
+                .about("add a new bookmark")
+                .arg(
+                    Arg::with_name("name")
+                        .short("n")
+                        .long("name")
+                        .required(true)
+                        .takes_value(true)
+                        .help("name of the bookmark"),
+                )
+                .arg(
+                    Arg::with_name("url")
+                        .short("u")
+                        .long("url")
+                        .required(true)
+                        .takes_value(true)
+                        .help("url of the bookmark"),
+                )
+                .arg(
+                    Arg::with_name("tags")
+                        .short("t")
+                        .long("tags")
+                        .required(true)
+                        .multiple(true)
+                        .takes_value(true)
+                        .help("tags of the bookmark"),
+                )
+                .arg(
+                    Arg::with_name("force")
+                        .long("force")
+                        .required(false)
+                        .takes_value(false)
+                        .help("override a bookmark if one already exists"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("edit").about("edit a bookmark").arg(
+                Arg::with_name("name")
+                    .short("n")
+                    .long("name")
+                    .required(true)
+                    .takes_value(true)
+                    .help("name of the bookmark"),
+            ),
+        )
+        .subcommand(
+            SubCommand::with_name("delete")
+                .about("delete a bookmark")
+                .arg(
+                    Arg::with_name("name")
+                        .short("n")
+                        .long("name")
+                        .required(true)
+                        .takes_value(true)
+                        .help("name of the bookmark"),
+                ),
+        )
+        .get_matches();
 
     // Run the application.
-    if let Err(e) = run() {
+    if let Err(e) = run(args) {
         eprintln!("{}", e);
         std::process::exit(2);
     }
     std::process::exit(0);
 }
 
-fn run() -> Result<()> {
-    // Load bookmarks file.
-    let home_path = dirs::home_dir().with_context(|| format!("Could not find home directory."))?;
-    let bookmarks_path = &home_path.join(".docket");
-    let bookmarks_contents = &std::fs::read_to_string(bookmarks_path)
-        .with_context(|| format!("No bookmarks found at '{}'.", bookmarks_path.display()))?;
-
-    // Parse bookmarks file.
-    let bookmarks_parsed_docs = YamlLoader::load_from_str(bookmarks_contents)?;
-    let bookmarks_parsed_doc = &bookmarks_parsed_docs[0];
-
-    // Pull out bookmarks.
-    let bookmarks = &bookmarks_parsed_doc["bookmarks"];
-    if bookmarks.is_badvalue() {
-        anyhow::bail!(
-            "No 'bookmarks' key found in '{}'.",
-            bookmarks_path.display()
-        );
-    }
-    let bookmarks_hash = &bookmarks.as_hash().with_context(|| {
-        format!(
-            "No 'bookmarks' hash map found in '{}'.",
-            bookmarks_path.display(),
-        )
-    })?;
-
-    // Print out every bookmark with corresponding context.
-    for bookmark in bookmarks_hash.iter() {
-        let name = bookmark.0.as_str().with_context(|| {
-            format!(
-                "The 'bookmarks' hash map is malformed in '{}'.",
-                bookmarks_path.display(),
-            )
-        })?;
-        let url = bookmark.1["url"].as_str().with_context(|| {
-            format!(
-                "Bookmark '{}' malformed at 'url' key in '{}'.",
-                name,
-                bookmarks_path.display()
-            )
-        })?;
-        let tags = bookmark.1["tags"].as_vec().with_context(|| {
-            format!(
-                "Bookmark '{}' malformed at 'tags' key in '{}'.",
-                name,
-                bookmarks_path.display()
-            )
-        })?;
-        let mut tags_parsed = vec![];
-        for tag in tags {
-            let tag_parsed = tag.as_str().with_context(|| {
-                format!(
-                    "Bookmark '{}' expected strings at 'tags' key in '{}'.",
-                    name,
-                    bookmarks_path.display()
-                )
-            })?;
-            tags_parsed.push(tag_parsed);
-        }
-
-        println!("{}\t{}\t{}", name, tags_parsed.join(","), url);
+/*
+ * Run application according to command line interface arguments.
+ */
+fn run(args: clap::ArgMatches) -> Result<()> {
+    if let Some(_) = args.subcommand_matches("view") {
+        command_view(args)?;
+    } else if let Some(_) = args.subcommand_matches("list-tags") {
+        command_list_tags()?;
+    } else if let Some(_) = args.subcommand_matches("add") {
+        command_add(args)?;
+    } else if let Some(_) = args.subcommand_matches("edit") {
+        command_edit()?;
+    } else if let Some(_) = args.subcommand_matches("delete") {
+        command_delete()?;
     }
 
     return Ok(());
+}
+
+/*
+ * Command to view bookmarks.
+ */
+fn command_view(args: clap::ArgMatches) -> Result<()> {
+    // Load config.
+    let config_path = &std::path::PathBuf::from(
+        shellexpand::tilde(args.value_of("config").unwrap()).into_owned(),
+    );
+    let config = load_config(config_path)?;
+
+    // Print out every bookmark with corresponding context.
+    for bookmark in config.bookmarks.iter() {
+        println!(
+            "{}\t{}\t{}",
+            bookmark.0,
+            bookmark.1.tags.join(","),
+            bookmark.1.url
+        );
+    }
+    return Ok(());
+}
+
+/*
+ * Command to list out bookmark tags.
+ */
+fn command_list_tags() -> Result<()> {
+    // TODO@nw:
+    return Ok(());
+}
+
+/*
+ * Command to add a bookmark.
+ */
+fn command_add(args: clap::ArgMatches) -> Result<()> {
+    let args_add = args.subcommand_matches("add").unwrap();
+
+    // Load config.
+    let config_path = &std::path::PathBuf::from(
+        shellexpand::tilde(args.value_of("config").unwrap()).into_owned(),
+    );
+    let mut config = load_config(config_path)?;
+
+    // Add bookmark.
+    let name = args_add.value_of("name").unwrap();
+    let url = args_add.value_of("url").unwrap();
+    let tags = args_add.values_of("tags").unwrap();
+
+    // Check if it already exists.
+    if config.bookmarks.contains_key(name) {
+        if !args_add.is_present("force") {
+            anyhow::bail!(
+                "Bookmark already exists with name '{}'. Use '--force' to override.",
+                name
+            );
+        } else {
+            config.bookmarks.remove(name);
+        }
+    }
+
+    // Insert the new bookmark.
+    config.bookmarks.insert(
+        String::from(name),
+        ConfigBookmark {
+            url: String::from(url),
+            tags: tags.map(|t| String::from(t)).collect(),
+        },
+    );
+
+    // Save.
+    std::fs::write(config_path, serde_yaml::to_string(&config)?)?;
+
+    return Ok(());
+}
+
+/*
+ * Command to edit a bookmark.
+ */
+fn command_edit() -> Result<()> {
+    // TODO@nw: Edit
+    //          docket edit --name "Some bookmark"
+    return Ok(());
+}
+
+/*
+ * Command to delete a bookmark.
+ */
+fn command_delete() -> Result<()> {
+    // - TODO@nw: Delete
+    //            docket delete --name "Some Bookmark"
+    return Ok(());
+}
+
+/*
+ * Loads a docket configuration file.
+ */
+fn load_config(config_path: &std::path::PathBuf) -> Result<Config> {
+    // Load config file.
+    let contents = std::fs::read_to_string(config_path)
+        .with_context(|| format!("No config found at '{}'.", config_path.display()))?;
+
+    // Parse config file.
+    let config: Config = serde_yaml::from_str(&contents)?;
+
+    return Ok(config);
 }
